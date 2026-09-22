61881e5b638783941bdf15cd06ccc603d13b6c15
Yann Le Foll <ylefoll@ylefoll.eu>
2026-09-22 21:40:16 +0000

    webapp: render crafting items in a PrimeVue DataTable

--- packages/webapp/src/views/CraftingView.vue
+++ packages/webapp/src/views/CraftingView.vue
@@ -2,22 +2,36 @@
 
 import {onMounted, ref} from "vue";
 import {getFightChicken, getItems} from "@/api/client";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 
 const items = ref([])
 
 onMounted(async () => {
-    getItems().then((data) => {
-      items.value = data.data
-    })
+  getItems({
+    query: {
+      skill: "weaponcrafting",
+      level: 30
+    }
+  }).then((data) => {
+    items.value = data.data
+  })
 })
 
 </script>
 
 <template>
   <h1>Crafting</h1>
-  <pre>
-    {{ items }}
-  </pre>
+
+  <DataTable :value="items">
+    <Column field="name" header="Name"/>
+    <Column field="level" header="Level"/>
+    <Column field="description" header="Description"/>
+
+  </DataTable>
+<!--  <pre>-->
+<!--    {{ items }}-->
+<!--  </pre>-->
 </template>
 
 <style scoped>
